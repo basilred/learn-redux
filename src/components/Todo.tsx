8c495615f2dbeb9a9489f8e5b93c110743bd396f
Vassily Krasnov <mykrasnov@gmail.com>
2019-01-30 23:56:46 +0300

    Replace simple string className with bem classname

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { cn } from '@bem-react/classname';
 import './Todo.css';
+
+const todoCn = cn('Todo');
 
 const Todo = ({ onClick, completed, text }: { onClick: any, completed: boolean, text: string }) => (
   <li
-    className={completed ? "Todo Todo_completed" : "Todo"}
+    className={completed ? todoCn({ completed: true }) : todoCn()}
     onClick={onClick}
   >
     {text}
